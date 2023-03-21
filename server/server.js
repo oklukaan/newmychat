c7a866aba941d59cb4bb651044fcead25f49aaea
oklukaan <kaan.oklu@hotmail.com>
2023-03-22 00:19:36 +0300

    qqqq

--- server/server.js
+++ server/server.js
@@ -98,18 +98,18 @@ app.post('/', async(req,res)=>{
 
         
     
-        const content=response.data.choices[0].text;
+        // const content=response.data.choices[0].text;
 
-        try {
-            const mywordpres=await fetch("http://127.0.0.1:80/wordpress/wp-json/wp/v2/posts", {
-            method: "POST",
-            headers: headers,
-            body: JSON.stringify({ title: "Hello from kaan", content: `<!-- wp:paragraph -->nmyasasdHello<!-- /wp:paragraph -->`, status: "draft" })
-          });
+        // try {
+        //     const mywordpres=await fetch("http://127.0.0.1:80/wordpress/wp-json/wp/v2/posts", {
+        //     method: "POST",
+        //     headers: headers,
+        //     body: JSON.stringify({ title: "Hello from kaan", content: `<!-- wp:paragraph -->nmyasasdHello<!-- /wp:paragraph -->`, status: "draft" })
+        //   });
             
-        } catch (error) {
-            console.log(error.message)
-        }
+        // } catch (error) {
+        //     console.log(error.message)
+        // }
         
 
         // fs.appendFile('index.php',content,err=>{
