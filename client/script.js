131524bd3267fbb8a7c5cad7d13a089ce2c1deaf
oklukaan <kaan.oklu@hotmail.com>
2023-03-21 22:34:11 +0300

    qwe

--- client/script.js
+++ client/script.js
@@ -92,7 +92,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://newmychat.onrender.com/', {
+    const response = await fetch('http://localhost:5000', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
